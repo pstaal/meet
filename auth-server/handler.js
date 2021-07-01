7374485391bbc20890638e4ad8ffd2645ad9b903
pstaal <peter.staal@gmail.com>
2021-07-01 14:02:02 +0200

    add getcalendarevents function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -99,3 +99,52 @@ module.exports.getAccessToken = async (event) => {
       };
     });
 };
+
+
+module.exports.getCalendarEvents = async (event) {
+
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject) => {
+
+    calendar.events.list(
+      {
+        calendarId: calendar_id,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    )
+    .then((results) => {
+      // Respond with OAuth token 
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ events: results.data.items }),
+      };
+    })
+    .catch((err) => {
+      // Handle error
+      console.error(err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err),
+      };
+    });
+  });
+
+}
