e01ded7b0310d42526d127033aa59c21d53eac6e
pstaal <peter.staal@gmail.com>
2021-07-02 10:58:15 +0200

    add EventList Component in App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 
 import React, { Component } from 'react';
 import './App.css';
+import EventList from './EventList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <EventList />
       </div>
     );
   }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
+import EventList from '../EventList';
 
 describe('<App /> component', () => {
 
