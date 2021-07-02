32c1b8c20a4f83c705011d4de8b6cc0c6b018e85
pstaal <peter.staal@gmail.com>
2021-07-02 13:56:17 +0200

    add event handler to input in citysearch component

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -5,6 +5,11 @@ class CitySearch extends Component {
     query: '',
   }
 
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ query: value });
+  }
+
   render() {
     return (
       <div className="CitySearch">
@@ -12,6 +17,7 @@ class CitySearch extends Component {
         type="text"
         className="city"
         value={this.state.query}
+        onChange={this.handleInputChanged}
       />
       <ul className="suggestions">
       </ul>
