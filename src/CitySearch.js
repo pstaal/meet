78defb4a615ed07681a6b10c7bead336141f0e11
pstaal <peter.staal@gmail.com>
2021-07-02 14:26:10 +0200

    change handle input in citysearch to set suggestions state properly

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -9,8 +9,14 @@ class CitySearch extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    this.setState({ query: value });
-  }
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    this.setState({
+      query: value,
+      suggestions,
+    });
+  };
 
   render() {
     return (
