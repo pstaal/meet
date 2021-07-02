5eab349f652d465b459503baeb855ce1fa9b5059
pstaal <peter.staal@gmail.com>
2021-07-02 11:36:26 +0200

    refactoring

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
   render() {
     return (
-      <div>
+      <div className="CitySearch">
       </div>
     );
   }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -7,14 +7,16 @@ import EventList from '../EventList';
 import CitySearch from '../CitySearch';
 
 describe('<App /> component', () => {
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
 
   test('render list of events', () => {
-    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   });
 
   test('render CitySearch', () => {
-    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 
