433b3bd7cc9e80428c3a72d844e75e4996f65ee7
pstaal <peter.staal@gmail.com>
2021-07-02 14:05:33 +0200

    add test in citysearch to see if suggestions are rendered correctly

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -29,4 +29,15 @@ describe('<CitySearch /> component', () => {
     expect(CitySearchWrapper.state('query')).toBe('Berlin');
   });
 
+  test('render list of suggestions correctly', () => {
+    const locations = extractLocations(mockData);
+    const CitySearchWrapper = shallow(<CitySearch />);
+    CitySearchWrapper.setState({ suggestions: locations });
+    const suggestions = CitySearchWrapper.state('suggestions');
+    expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length + 1);
+    for (let i = 0; i < suggestions.length; i += 1) {
+      expect(CitySearchWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i]);
+    }
+  });
+
 });
