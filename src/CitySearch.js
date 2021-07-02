894af9dad0c3b7adf8dbb447eec1394310853f55
pstaal <peter.staal@gmail.com>
2021-07-02 11:32:43 +0200

    create citysearch component

---
+++ src/CitySearch.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CitySearch extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default CitySearch;
