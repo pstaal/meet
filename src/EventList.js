d8d7189fc4e5ba6b902eba99b9d690a6406b758c
pstaal <peter.staal@gmail.com>
2021-07-02 11:04:42 +0200

    replace div for ul element in eventlist component

--- src/EventList.js
+++ src/EventList.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class EventList extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <ul className="EventList">
+      </ul>
     );
   }
 }
