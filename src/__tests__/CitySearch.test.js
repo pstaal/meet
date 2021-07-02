87d7c5111623b00182f8255903a708b3cdab61b2
pstaal <peter.staal@gmail.com>
2021-07-02 13:51:34 +0200

    add test to see if input changes in citysearch, the state also changes

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -19,4 +19,14 @@ describe('<CitySearch /> component', () => {
     expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
   });
 
+  test('change state when text input changes', () => {
+    const CitySearchWrapper = shallow(<CitySearch />);
+    CitySearchWrapper.setState({
+      query: 'Munich'
+    });
+    const eventObject = { target: { value: 'Berlin' }};
+    CitySearchWrapper.find('.city').simulate('change', eventObject);
+    expect(CitySearchWrapper.state('query')).toBe('Berlin');
+  });
+
 });
