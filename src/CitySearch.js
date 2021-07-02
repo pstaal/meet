59dd0a08930852b88287efd561196c3397efda46
pstaal <peter.staal@gmail.com>
2021-07-02 15:29:36 +0200

    add onclick handler in citysearch component to handle clicks on suggestions

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -18,6 +18,12 @@ class CitySearch extends Component {
     });
   };
 
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion
+    });
+  };
+
   render() {
     return (
       <div className="CitySearch">
@@ -28,12 +34,15 @@ class CitySearch extends Component {
         onChange={this.handleInputChanged}
       />
       <ul className="suggestions">
-      {this.state.suggestions.map((suggestion) => (
-        <li key={suggestion}>{suggestion}</li>
-        ))}
-        <li key='all'>
-        <b>See all cities</b>
-        </li>
+        {this.state.suggestions.map((suggestion) => (
+          <li
+            key={suggestion}
+            onClick={() => this.handleItemClicked(suggestion)}
+          >{suggestion}</li>
+      ))}
+          <li>
+            <b>See all cities</b>
+          </li>
       </ul>
       </div>
     );
