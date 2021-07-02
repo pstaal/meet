f861d34f00a75937091f77ace1902f53cfbba676
pstaal <peter.staal@gmail.com>
2021-07-02 11:34:08 +0200

    add citysearch component to app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
+import CitySearch from './CitySearch';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <CitySearch />
         <EventList />
       </div>
     );
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
 
 describe('<App /> component', () => {
 
