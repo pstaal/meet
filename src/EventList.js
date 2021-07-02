46e6584cfb0ffe87c62701b10ba5b49b79c05be6
pstaal <peter.staal@gmail.com>
2021-07-02 11:13:12 +0200

    rewrite eventlist component to receive props and render events from props

--- src/EventList.js
+++ src/EventList.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import Event from './Event';
 
 class EventList extends Component {
+  const { events } = this.props;
+
   render() {
     return (
       <ul className="EventList">
+        {events.map(event =>
+        <li>
+          <Event event={event} />
+        </li>
+      )}
       </ul>
     );
   }
--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import EventList from '../EventList';
+import Event from '../Event';
 
 describe('<EventList /> component', () => {
   test('render correct number of events', () => {
