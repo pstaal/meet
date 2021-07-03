77c616ec62537d59c274973d4903c03709ce00e1
pstaal <peter.staal@gmail.com>
2021-07-03 20:13:49 +0200

    made number of events test pass in app.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CitySearch />
+        <NumberOfEvents />
         <EventList />
       </div>
     );
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -5,6 +5,7 @@ import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents';
 
 describe('<App /> component', () => {
   let AppWrapper;
