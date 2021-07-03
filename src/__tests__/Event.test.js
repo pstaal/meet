e42192d78e0fec3c823b3c3f4222b7e0517d3d47
pstaal <peter.staal@gmail.com>
2021-07-03 10:48:22 +0200

    add tests to check for contents in event components

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -16,6 +16,35 @@ describe('<Event /> component', () => {
   test('render the right amount of descriptions', () => {
     expect(EventWrapper.find('.event_description')).toHaveLength(mockEventData.length);
   });
+  
+  test('render titles with the right content', () => {
+    for (let i = 0; i < mockEventData.length; i += 1) {
+      expect(EventWrapper.find('h1').at(i).text()).toBe(mockEventData[i].summary);
+    }
+  });
 
+  test('render descriptions with dates', () => {
+    for (let i = 0; i < mockEventData.length; i += 1) {
+      expect(EventWrapper.find('.event_description').at(i).text()).toContain(mockEventData[i].start.dateTime);
+    }
+  });
+
+  test('render descriptions with times', () => {
+    for (let i = 0; i < mockEventData.length; i += 1) {
+      expect(EventWrapper.find('.event_description').at(i).text()).toContain(mockEventData[i].start.timeZone);
+    }
+  });
+
+  test('render descriptions with summary', () => {
+    for (let i = 0; i < mockEventData.length; i += 1) {
+      expect(EventWrapper.find('.event_description').at(i).text()).toContain(mockEventData[i].summary);
+    }
+  });
+
+  test('render descriptions with summary', () => {
+    for (let i = 0; i < mockEventData.length; i += 1) {
+      expect(EventWrapper.find('.event_description').at(i).text()).toContain(mockEventData[i].location);
+    }
+  });
 
 });
