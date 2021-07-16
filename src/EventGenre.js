60c4834d84310e4e1beb3a1a9dc4899f6866980a
pstaal <peter.staal@gmail.com>
2021-07-16 10:27:40 +0200

    debugging

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -28,7 +28,7 @@ return (
     labelLine={false}
     outerRadius={80}
     fill="#8884d8"
-    dataKey={value}
+    dataKey="value"
     label={({name, percent}) => `${name} ${(percent * 100).toFixed(0)}%`}
     />
   </PieChart>
